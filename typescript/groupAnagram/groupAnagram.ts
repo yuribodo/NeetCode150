0948dd6b8e45843945008072a7a807053c3c2189
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-06-18 16:14:19 -0400

    adjustment

--- typescript/groupAnagram/groupAnagram.ts
+++ typescript/groupAnagram/groupAnagram.ts
@@ -1,19 +1,18 @@
-let strs = ["act","pots","tops","cat","stop","hat"];
+function groupAnagrams(strs: string[]): string[][] {
+    let res = {};
 
-let res = {};
-
-for (let s of strs) {  // Use 'for...of' to iterate over elements of the array
-    let count = new Array(26).fill(0);  // Initialize the count array with zeros
-    for (let c of s) {  // Use 'for...of' to iterate over characters of the string
-        count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;  // Update the count for the character
+    for (let s of strs) {  // Use 'for...of' to iterate over elements of the array
+        let count = new Array(26).fill(0);  // Initialize the count array with zeros
+        for (let c of s) {  // Use 'for...of' to iterate over characters of the string
+            count[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;  // Update the count for the character
+        }
+        let key = count.join(',');  // Create a key from the count array
+        if (!res[key]) {
+            res[key] = [];
+        }
+        res[key].push(s);
     }
-    let key = count.join(',');  // Create a key from the count array
-    if (!res[key]) {
-        res[key] = [];
-    }
-    res[key].push(s);
-}
+    let response: string[][] = Object.values(res)
+    return response
 
-let response: string[][] =  Object.values(res)
-
-console.log(response);
+};
