5430af9fffec3b8d79d8dacc6cc24f8acff5f879
Mario Yuri Mota Lara <yemotalara@gmail.com>
2024-06-27 18:33:35 -0400

    invert tree

---
+++ javascript/inverttree.js
@@ -0,0 +1,38 @@
+/* 
+Problema:
+You are given the root of a binary tree root. Invert the binary tree and return its root.
+
+Example 1:
+Input: root = [1,2,3,4,5,6,7]
+
+Output: [1,3,2,7,6,5,4]
+
+
+Example 2:
+Input: root = [3,2,1]
+
+Output: [3,1,2]
+
+*/
+
+
+
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     constructor(val = 0, left = null, right = null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {TreeNode}
+     */
+    invertTree(root) {}
+}
+
--- typescript/duplicate/duplicate.ts
+++ typescript/duplicate/duplicate.ts
@@ -1,19 +1,10 @@
-let message: string = 'Hello World';
-console.log(message);
-
-let nums = [1, 2, ,3 ,2 ]
-
-
-nums.sort(function(a, b){return a-b})
-
-console.log(nums)
-
-const tamanho = nums.length -1
-
-for (let i = 0; i < tamanho; i++) {
-    if(nums[i] == nums[i+1]) {
-        console.log("repetiu")
-    }
-    console.log("nao repete")
-
-}   
+function containsDuplicate(nums: number[]): boolean {
+    nums.sort(function(a, b){return a-b})
+    const tamanho = nums.length -1
+    for (let i = 0; i < tamanho; i++) {
+        if(nums[i] == nums[i+1]) {
+            return true
+        }
+    }   
+    return false
+};
